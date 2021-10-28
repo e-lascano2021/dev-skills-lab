b94e9b04f85bfd755a97197e661611d6f81d1ff8
e.lascano <emilas5678@gmail.com>
2021-10-28 12:50:46 -0400

    second commit

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import methodOverride from 'method-override'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
-import { router as usersRouter } from './routes/users.js'
+import { router as skillsRouter } from './routes/skills.js'
 
 // set up app
 const app = express()
@@ -18,8 +19,12 @@ app.set(
   path.join(path.dirname(fileURLToPath(import.meta.url)), 'views')
 )
 app.set('view engine', 'ejs')
-
 // middleware
+app.use(function(req, res, next){
+  console.log('HELLOOOOOOO')
+  req.time = new Date().toLocaleTimeString()
+  next()
+})
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -32,7 +37,7 @@ app.use(
 
 // mounted routers
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/skills', skillsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
